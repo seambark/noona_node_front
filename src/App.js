baad95027031468ea9e0dfc9af6c2588b3445c2a
Seam Park <run128@naver.com>
2024-05-25 03:11:28 +0900

    todolist-front

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -6,8 +7,81 @@ import TodoBoard from "./components/TodoBoard";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import api from "./utils/api";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  const getTasks = async () => {
+    const response = api.get("/tasks");
+
+    const getData = () => {
+      response.then((list) => {
+        setTodoList(list.data.data);
+      });
+    };
+    getData();
+  };
+
+  const addTask = async () => {
+    try {
+      const response = await api.post("/tasks", {
+        task: todoValue,
+        isComplete: false,
+      });
+
+      if (response.status === 200) {
+        console.log("추가 성공");
+        setTodoValue("");
+        getTasks();
+      } else {
+        throw new Error("내용 추가에 실패하였습니다.");
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+
+  const updateTask = async (id, isComplete) => {
+    const response = await api.put(`/tasks/${id}`, {
+      isComplete: isComplete === false ? true : false,
+    });
+
+    try {
+      if (response.status === 200) {
+        console.log("업데이트 성공");
+        setTodoValue("");
+        getTasks();
+      } else {
+        throw new Error("업데이트에 실패하였습니다.");
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+
+  const deleteTask = async (id) => {
+    console.log(id);
+    const response = await api.delete(`/tasks/${id}`);
+
+    try {
+      if (response.status === 200) {
+        console.log("삭제 성공");
+        setTodoValue("");
+        getTasks();
+      } else {
+        throw new Error("삭제에 실패하였습니다.");
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
   return (
     <Container>
       <Row className="add-item-row">
@@ -16,14 +90,22 @@ function App() {
             type="text"
             placeholder="할일을 입력하세요"
             className="input-box"
+            value={todoValue}
+            onChange={(e) => setTodoValue(e.target.value)}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className="button-add">추가</button>
+          <button className="button-add" onClick={addTask}>
+            추가
+          </button>
         </Col>
       </Row>
 
-      <TodoBoard />
+      <TodoBoard
+        todoList={todoList}
+        updateTask={updateTask}
+        deleteTask={deleteTask}
+      />
     </Container>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-const TodoItem = () => {
+const TodoItem = ({ item, updateTask, deleteTask }) => {
   return (
     <Row>
       <Col xs={12}>
-        <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
+        <div
+          className={`todo-item ${
+            item?.isComplete === false ? `` : `item-complete`
+          }`}
+        >
+          <div className="todo-content">{item?.task}</div>
 
           <div>
-            <button className="button-delete">삭제</button>
-            <button className="button-delete">끝남</button>
+            <button
+              className="button-delete"
+              onClick={() => updateTask(item?._id, item?.isComplete)}
+            >
+              {item?.isComplete === false ? `종료` : `재시작`}
+            </button>
+            <button
+              className="button-delete"
+              onClick={() => deleteTask(item?._id)}
+            >
+              삭제
+            </button>
           </div>
         </div>
       </Col>
